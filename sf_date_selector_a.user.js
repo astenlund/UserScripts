d393fb22086301320d86b92fbd01aeab47316f89
Andreas Stenlund <a.stenlund@gmail.com>
2016-08-02 23:39:00 +0200

    Add desktop notification

--- sf_date_selector_a.user.js
+++ sf_date_selector_a.user.js
@@ -1,14 +1,45 @@
 // ==UserScript==
-// @name         SF Date Selector
+// @name         SF Date Selector A
 // @namespace    fork-scripts
-// @version      0.1
-// @description  Select a particular date on the SF booking page
+// @version      0.2
+// @description  try to take over the world!
 // @author       Andreas Stenlund
-// @match        http://www.sf.se/biljetter/bokningsflodet/valj-forestallning/*
+// @match        http://www.sf.se/biljetter/bokningsflodet/valj-forestallning/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    $("ul li a[data-movielistfilterdate='05-08']").click();
+
+    var title = 'Jason Bourne';
+
+    var player = document.createElement('audio');
+    player.preload = 'auto';
+    player.src = 'https://dl.dropboxusercontent.com/u/2355432/tada.wav';
+
+    if (Notification.permission !== 'granted') {
+        Notification.requestPermission();
+    }
+
+    $("ul.customSelectBoxList li a[data-movielistfilterdate='05-08']").click();
+
+    var elem = $("#BookingList").find('li:contains(' + title + ')');
+    if (!elem.hasClass('displayNone')) {
+        player.play();
+
+        var notification = new Notification(title + ' går nu att boka!', {
+            icon: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Svensk_Filmindustri_logo.png',
+            body: 'Klicka här för att komma till bokningssidan',
+            requireInteraction: true
+        });
+
+        notification.onclick = function () {
+            notification.close();
+            window.open('http://www.sf.se/biljetter/bokningsflodet/valj-forestallning/?MoviePageId=18037');
+        };
+    } else {
+        setTimeout(function() {
+            window.location.reload(1);
+        }, 5000);
+    }
 })();

f70dcc9ed51b66e70f0c3a5ed94c244e9ab211da
Andreas Stenlund <a.stenlund@gmail.com>
2017-07-16 20:20:55 +0200

    Wait 2 seconds before executing script

--- nothing_fancy.user.js
+++ nothing_fancy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nothing Fancy - Movies on Google Play
 // @namespace    fork-scripts
-// @version      0.4
+// @version      0.5
 // @description  Remove expensive movies from Google Play
 // @downloadURL  https://github.com/astenlund/UserScripts/raw/master/nothing_fancy.user.js
 // @author       Andreas Stenlund
@@ -12,14 +12,18 @@
 (function() {
     'use strict';
 
-    $("div.card").each(function(i, cardDiv) {
-        var priceButton = $(cardDiv).find("button.price");
-        var priceSpan = $(priceButton).find("span.display-price");
-        var price = parseInt(priceSpan.html().split(",")[0]);
-        if (price > 50) {
-            console.log("Expensive: " + $(cardDiv).find("a.title").attr("title") + " (" + price + " kr)");
-            $(priceButton).css("font-weight", "bold");
-            $(priceButton).css("text-decoration", "underline");
-        }
-    });
+    var main = function() {
+        $("div.card").each(function(i, cardDiv) {
+            var priceButton = $(cardDiv).find("button.price");
+            var priceSpan = $(priceButton).find("span.display-price");
+            var price = parseInt(priceSpan.html().split(",")[0]);
+            if (price > 50) {
+                console.log("Expensive: " + $(cardDiv).find("a.title").attr("title") + " (" + price + " kr)");
+                $(priceButton).css("font-weight", "bold");
+                $(priceButton).css("text-decoration", "underline");
+            }
+        });
+    };
+
+    setTimeout(main, 2000);
 })();

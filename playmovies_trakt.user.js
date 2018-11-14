aba2fb003124c8f55af52b03cea5c3b3c366ee0a
Andreas Stenlund <a.stenlund@gmail.com>
2018-11-14 15:20:52 +0100

    Add Play Movies userscript for Trakt.tv

---
+++ playmovies_trakt.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Play Movies UserScript for Trakt.tv
+// @namespace   fork-scripts
+// @description Find a movie on Play Movies
+// @downloadURL https://github.com/astenlund/UserScripts/raw/master/trakt_imdb.user.js
+// @grant       none
+// @version     0.1
+// @match       *://trakt.tv/movies/*
+// ==/UserScript==
+
+var main = function() {
+    var movie_title = $('meta[property="og:title"]').attr('content');
+    var search_url = 'https://play.google.com/store/search?q=' + movie_title + '&c=movies';
+
+    $('ul.external li').append('<a target="_blank" href="' + search_url + '" data-original-title title>Play Movies</a>');
+}
+
+main();

2375d0490386da170a0ca5c589d60c2ab3286e10
Andreas Stenlund <andreas.stenlund@ticket.se>
2020-06-28 16:25:44 +0200

    Update Rotten Tomates userscript to handle page loads from clicking links

--- rottentomatoes_trakt.user.js
+++ rottentomatoes_trakt.user.js
@@ -4,16 +4,40 @@
 // @description Find a movie on Rotten Tomatoes
 // @downloadURL https://github.com/astenlund/UserScripts/raw/master/rottentomatoes_trakt.user.js
 // @grant       none
-// @version     0.1
-// @match       *://trakt.tv/movies/*
-// @match       *://trakt.tv/shows/*
+// @version     0.2
+// @match       *://trakt.tv
+// @match       *://trakt.tv/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var title = $('meta[property="og:title"]').attr('content');
-    var search_url = 'https://www.rottentomatoes.com/search?search=' + title;
+    var addRT = function() {
+        var url = document.location;
 
-    $('<a target="_blank" href="' + search_url + '" data-original-title title>Rotten Tomatoes</a>').insertBefore('#info-wrapper .sidebar ul.external li a:contains("IMDB")');
+        if (!/.+\:\/\/trakt\.tv\/movies\/.+/.test(url) && !/.+\:\/\/trakt\.tv\/shows\/.+/.test(url)) {
+            return;
+        }
+
+        var imdbLmnt = $('#info-wrapper .sidebar ul.external li a:contains("IMDB")')[0];
+        if (imdbLmnt == undefined) {
+            return;
+        }
+
+        var rtLmnt = $('#info-wrapper .sidebar ul.external li a:contains("Rotten Tomatoes")')[0];
+        if (rtLmnt != undefined) {
+            return;
+        }
+
+        var title = $('meta[property="og:title"]').attr('content');
+        var search_url = 'https://www.rottentomatoes.com/search?search=' + title;
+
+        $('<a target="_blank" href="' + search_url + '" data-original-title title>Rotten Tomatoes</a>').insertBefore(imdbLmnt);
+    };
+
+    window.onclick = function(e) {
+        addRT();
+    };
+
+    addRT();
 })();

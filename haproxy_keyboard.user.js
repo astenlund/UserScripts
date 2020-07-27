a6b607a4a6ad3c77566142fffcc1fb0a2f114739
Andreas Stenlund <a.stenlund@gmail.com>
2020-07-27 10:30:03 +0200

    Add URL matches to HAProxy keyboard shortcuts script

--- haproxy_keyboard.user.js
+++ haproxy_keyboard.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         HAProxy keyboard shortcuts
 // @namespace    fork-scripts
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @downloadURL  https://github.com/astenlund/UserScripts/raw/master/haproxy_keyboard.user.js
 // @author       Andreas Stenlund
 // @match        *://tckbalancete1.ticket.se:8080/*
+// @match        *://tckbalancetest1.ticket.se/*
+// @match        *://tckbalance1.ticket.se:8080/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==

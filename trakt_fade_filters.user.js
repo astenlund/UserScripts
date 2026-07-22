aaa4034302b04c64184850aafa0165d678bdff35
Andreas Stenlund <a.stenlund@gmail.com>
2026-07-23 01:54:06 +0200

    fix(trakt): fade summary-card details like footers, bump to 1.3

--- trakt_fade_filters.user.js
+++ trakt_fade_filters.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trakt Web: fade filters
 // @namespace    fork-scripts
-// @version      1.2
+// @version      1.3
 // @description  Restores fade/dim filtering in the new Trakt Web design: adds a Fade section to the filter pane and fades watched/started/watchlisted/listed posters, with hover-to-reveal.
 // @author       Andreas Stenlund <a.stenlund@gmail.com>
 // @downloadURL  https://github.com/astenlund/UserScripts/raw/master/trakt_fade_filters.user.js
@@ -329,21 +329,24 @@
     // !important throughout: the app's Svelte-scoped selectors (e.g.
     // .trakt-card-cover.svelte-x) beat plain class rules on specificity.
     style.textContent = `
-      .${FADE_CLASS} .trakt-card-cover, .${FADE_CLASS} .trakt-card-footer {
+      .${FADE_CLASS} .trakt-card-cover, .${FADE_CLASS} .trakt-card-footer,
+      .${FADE_CLASS} .trakt-summary-card-details {
         transition: opacity 250ms ease, filter 250ms ease !important;
       }
       .${FADE_CLASS} .trakt-card-cover {
         opacity: 0.5 !important;
         filter: brightness(0.5) saturate(0.5) !important;
       }
-      .${FADE_CLASS} .trakt-card-footer {
+      .${FADE_CLASS} .trakt-card-footer,
+      .${FADE_CLASS} .trakt-summary-card-details {
         filter: brightness(0.5) !important;
       }
       .${FADE_CLASS}:hover .trakt-card-cover {
         opacity: 1 !important;
         filter: none !important;
       }
-      .${FADE_CLASS}:hover .trakt-card-footer {
+      .${FADE_CLASS}:hover .trakt-card-footer,
+      .${FADE_CLASS}:hover .trakt-summary-card-details {
         filter: none !important;
       }
     `;
